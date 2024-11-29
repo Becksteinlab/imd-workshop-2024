f4ca34d1a151932cb9004c429de8825602a7e0b8
ljwoods2 <ljwoods2@asu.edu>
2024-11-28 22:25:42 -0700

    in progress activity

--- activity/run.sh
+++ activity/run.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+## TMP
+source /usr/local/gromacs/bin/GMXRC
+
+## WAIT UNTIL PORT IS OPEN
+SECONDS=0
+while lsof -n -i :8889; do
+    echo "Waiting for port 8889 to open"
+    sleep 1
+
+    if [ $SECONDS -ge 30 ]; then
+        echo "Timeout reached. Port 8889 did not open."
+        exit 1
+    fi
+done
+
 mkdir -p sample_simulation
 cd sample_simulation
+
+## RUN GROMPP
 if [ ! -f sample_simulation/topol.tpr ]; then
 echo "Running grompp"
 gmx grompp -f imd.mdp -c start.gro -p topol.top -o >& grompp.out
 fi
 echo "Starting mdrun"
-# modify gromacs execution string according to available resources
-# here we use 2 parallel threads
-gmx mdrun -v -nt 2 -imdwait -imdport 8889
+
+## START MDRUN
+gmx mdrun -v -nt 1 -imdwait -imdport 8889
 cd ..
